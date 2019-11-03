15b29c2973bb899dc1aea5b47d651f0b82d2fb96
Benjamin Brachmann <benjamin.brachmann@icloud.com>
2019-11-04 00:53:58 +0100

    memory leak update
    - last try of getting the memory leak fixed, no success

--- scripts/experiments/experiment-2.js
+++ scripts/experiments/experiment-2.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs-extra')
 const chalk = require('chalk')
-const Browser = require('zombie')
+let Browser = require('zombie')
 
 /***************************************
  * SETTINGS
@@ -16,15 +16,43 @@ const settings = {
         tags: 'tags.txt'
     },
     requestTimeout: 500,
-    requestWaitTime: 30000
+    requestWaitTime: 30000,
+    memoryAvailableInMb: 1372 // default
 }
 
 /***************************************
  * HELPERS
  ***************************************/
+
+const reRequire = (moduleName) => {
+    const entry = require.cache[require.resolve(moduleName)]
+    entry.children.forEach((child) => {
+        delete require.cache[child.filename]
+    })
+    delete require.cache[moduleName]
+    if (global.gc) {
+        global.gc()
+    }
+    return require(moduleName)
+}
  
 const replaceHandle = (string, {name = '', value = ''}) => {
    return string.replace(`{{${name}}}`, value)
+}
+
+const getMemoryUsage = () => {
+    return Math.round(process.memoryUsage().heapUsed / (1024 * 1024))
+}
+
+const getMemoryUsageString = () => {
+    const usedMemoryInMb = Math.round((getMemoryUsage() / settings.memoryAvailableInMb) * 100)
+    if(usedMemoryInMb > 70) {
+        return chalk.red(`${usedMemoryInMb}%`)
+    } else if(usedMemoryInMb > 50) {
+        return chalk.yellow(`${usedMemoryInMb}%`)
+    } else {
+        return chalk.green(`${usedMemoryInMb}%`)
+    }
 }
 
 /***************************************
@@ -35,13 +63,15 @@ const makeCompleteUrl = (url, path) => {
     return `http://${url}/${path || ''}`
 }
 
-const openWebsite = () => {
+const openWebsite = ({url = settings.url, silent = false}) => {
     return new Promise((resolve, reject) => {
-        console.log(`[${chalk.cyan(settings.url)}] loading in headless browser`)
+        if(!silent) {
+            console.log(`[${chalk.cyan(url)}] loading in headless browser`)
+        }
         const browser = new Browser({
             waitDuration: settings.requestWaitTime
         })
-        browser.visit(makeCompleteUrl(settings.url), () => {
+        browser.visit(makeCompleteUrl(url), () => {
             browser.status === 200 ? resolve(browser) : reject(browser.status)
         })
     })
@@ -61,36 +91,43 @@ const saveExtractedTags = (tags) => {
     })
 }
 
-const extractTagsRecursively = (browser, currentPage, lastPage, resolve, reject) => {
-    browser.clickLink('#tags_list > div.pager.fr > a:last-child', () => {
-        if(browser.status === 200) {
-            const tags = browser.queryAll('#tags-browser > div > a').map(element => browser.text(element))
-            const percentage = Math.round(currentPage / lastPage * 100)
-            saveExtractedTags(tags)
-            console.log(`[${chalk.cyan(settings.url)}] extracted tags of page ${currentPage}/${lastPage} (P: ${chalk.cyan(percentage + '%')} T:${tags.length})`)
-            if(currentPage === lastPage) {
-                resolve()
+const extractTagsRecursively = (browser, currentPage, lastPage) => {
+    return new Promise((resolve, reject) => {
+        browser.deleteCookies()
+        browser.clickLink('#tags_list > div.pager.fr > a:last-child', () => {
+            if(browser.status === 200) {
+                const tags = browser.queryAll('#tags-browser > div > a').map(element => browser.text(element))
+                const percentage = Math.round(currentPage / lastPage * 100)
+                console.log(`[${chalk.cyan(settings.url)}] extracted tags of page ${currentPage}/${lastPage} (P: ${chalk.cyan(percentage + '%')} T:${tags.length} M:${getMemoryUsageString()})`)
+                resolve(tags)
+            } else {
+                console.error(`[${chalk.cyan(settings.url)}] error loading page ${chalk.yellow((currentPage))}/${lastPage} - ${chalk.red(browser.status)}`)
+                reject(browser.status)
             }
-            try {
-                extractTagsRecursively(browser, (currentPage + 1), lastPage, resolve, reject)
-            } catch(error) {
-                console.error(`[${chalk.cyan(settings.url)}] error extracting page ${chalk.orange((currentPage + 1))}/${lastPage}, trying again`)
-                extractTagsRecursively(browser, (currentPage + 1), lastPage, resolve, reject)
-            }
-        } else {
-            console.error(`[${chalk.cyan(settings.url)}] error loading page ${chalk.orange((currentPage))}/${lastPage} - ${chalk.red(browser.status)}`)
-            reject(browser.status)
-        }
+        })
     })
 }
 
 const extractTags = (browser) => {
     return new Promise((resolve, reject) => {
-        browser.visit(makeCompleteUrl(settings.url, replaceHandle(settings.paths.tags, {name: 'page', value: 1})), () => {
+        browser.visit(makeCompleteUrl(settings.url, replaceHandle(settings.paths.tags, {name: 'page', value: 1})), async () => {
             if(browser.status === 200) {
                 const tagPages = Number(browser.text('#tags_list > div.pager.fr > a:nth-child(7) > span'))
                 console.log(`[${chalk.cyan(settings.url)}] has ${chalk.cyan(tagPages)} pages with tag content`)
-                extractTagsRecursively(browser, 1, tagPages, resolve, reject)
+                for(let i = 1; i <= tagPages; i++) {
+                    if(getMemoryUsage() / settings.memoryAvailableInMb > 0.5) {
+                        // Memory usage > X%
+                        const currentUrl = browser.location._url.replace('https://', '')
+                        browser.window.close()
+                        browser = null
+                        Browser = null
+                        Browser = reRequire('zombie')
+                        browser = await openWebsite({url: currentUrl, silent: true})
+                    }
+                    const extractedTags = await extractTagsRecursively(browser, i, tagPages)
+                    saveExtractedTags(extractedTags)
+                }
+                resolve()
             } else {
                 reject(browser.status)
             }
