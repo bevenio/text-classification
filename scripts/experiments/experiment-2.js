ad081a8f286fdca1ec4e8353b7c74912a1c35680
Benjamin Brachmann <benjamin.brachmann@icloud.com>
2019-11-06 10:26:20 +0100

    - updated script
    script now downloads as long as not forbidden

--- scripts/experiments/experiment-2.js
+++ scripts/experiments/experiment-2.js
@@ -10,10 +10,12 @@ let Browser = require('zombie')
 const settings = {
     url: 'stackoverflow.com',
     paths: {
-        tags: 'tags?page={{page}}&tab=popular'
+        tags: 'tags?page={{page}}&tab=popular',
+        questions: 'questions?tab=newest&page={{page}}'
     },
     outputs: {
-        tags: 'tags.txt'
+        tags: 'tags.txt',
+        questions: 'stack_titles.txt'
     },
     requestTimeout: 500,
     requestWaitTime: 30000,
@@ -134,6 +136,102 @@ const extractTags = (browser) => {
         })
     })
 }
+/*************************************************
+ * OPEN THE POP PAGE AND EXTRACT TEXT AND TAGS
+ *************************************************/
+
+const getStore = () => {
+    return require('./store/store.json')
+}
+
+const setStore = (store) => {
+    fs.outputFileSync(
+        path.resolve(__dirname, './store/store.json'),
+        JSON.stringify(store, null, 1),
+        (error) => {
+            if(error) {
+                console.log(chalk.red(error))
+            }
+        }
+    )
+}
+
+const saveExtractedQuestions = (questions) => {
+    const filePath = path.resolve(__dirname, `./outputs/${settings.outputs.questions}`)
+    if (!fs.existsSync(filePath)) {
+        fs.outputFileSync(filePath, '')
+    }
+    const questionClassifierLines = questions.map(question => {
+        return `${question.tags.map(tag => `__label__${tag}`).join(' ')} ${question.title}`
+    })
+    fs.appendFile(filePath, questionClassifierLines.join('\n').concat('\n'), function (err) {
+        if (err) throw err
+    })
+}
+
+const extractQuestionTitlesRecursively = (browser, currentPage, lastPage) => {
+    return new Promise((resolve, reject) => {
+        browser.deleteCookies()
+        browser.clickLink('#mainbar > div.pager.fl > a:last-child', () => {
+            if(browser.status === 200) {
+                const percentage = (Math.round(currentPage / lastPage * 10000) /100)
+                const questions = browser.queryAll('div.summary').map(container => {
+                    return {
+                        title: browser.text('div.summary > h3 > a', container),
+                        tags: browser.queryAll('div.summary > div.tags > a', container).map(element => browser.text(element))
+                    }
+                })
+                console.log(`[${chalk.cyan(settings.url)}] extracted questions of page ${currentPage}/${lastPage} (P: ${chalk.cyan(percentage + '%')} Q:${questions.length} M:${getMemoryUsageString()})`)
+                resolve(questions)
+            } else {
+                console.error(`[${chalk.cyan(settings.url)}] error loading page ${chalk.yellow((currentPage))}/${lastPage} - ${chalk.red(browser.status)}`)
+                reject(browser.status)
+            }
+        })
+    })
+}
+
+const extractQuestionTitles = (browser) => {
+    return new Promise((resolve, reject) => {
+        browser.visit(makeCompleteUrl(settings.url, replaceHandle(settings.paths.questions, {name: 'page', value: 1})), () => {
+            const questionPages = Number(browser.text('#mainbar > div.pager.fl > a:nth-child(7) > span'))
+            if(browser.status === 200) {
+                const store = getStore()
+                browser.visit(makeCompleteUrl(settings.url, replaceHandle(settings.paths.questions, {name: 'page', value: store.nextPage})), async () => {
+                    if(browser.status === 200) {
+                        console.log(`[${chalk.cyan(settings.url)}] has ${chalk.cyan(questionPages)} pages with question content`)
+                        for(let i = store.nextPage; i <= questionPages; i++) {
+                            const extractedQuestions = await extractQuestionTitlesRecursively(browser, i, questionPages)
+                            saveExtractedQuestions(extractedQuestions)
+                            setStore({
+                                ...store,
+                                nextPage: (i + 2)
+                            })
+                            if(getMemoryUsage() / settings.memoryAvailableInMb > 0.8) {
+                                console.log(`Extracted questions from ${chalk.cyan(i - store.nextPage)} pages`)
+                                setStore({
+                                    nextPage: (i + 2),
+                                    restarts: (store.restarts + 1),
+                                    stats: {
+                                        questionsAverage: ((i - store.nextPage) + store.stats.questionsAverage * store.stats.questionsAverageAmount) / (store.stats.questionsAverageAmount + 1),
+                                        questionsAverageAmount: (store.stats.questionsAverageAmount + 1),
+                                        percentProcessed: (Math.round((i / questionPages) * 10000) / 100)
+                                    }
+                                })
+                                process.exit(0)
+                            }
+                        }
+                        resolve()
+                    } else {
+                        reject(browser.status)
+                    }
+                })
+            } else {
+                reject(browser.status)
+            }
+        })
+    })
+}
 
 /***************************************
  * PROGRAMM
@@ -142,9 +240,15 @@ const extractTags = (browser) => {
 openWebsite(settings)
     .then(browser => {
         console.log(`[${chalk.cyan(settings.url)}] responded with ${chalk.cyan(browser.status)}`)
-        extractTags(browser)
-            .then((tags) => {
-                console.log(`[${chalk.cyan(settings.url)}] extracted ${chalk.green('all')} tags`)
+        // extractTags(browser)
+        //     .then((tags) => {
+        //         console.log(`[${chalk.cyan(settings.url)}] extracted ${chalk.green('all')} tags`)
+        //     }).catch(code => {
+        //         console.error(`[${chalk.cyan(settings.url)}] responded with ${chalk.red(code)}`)
+        //     })
+        extractQuestionTitles(browser)
+            .then(() => {
+                console.log(`[${chalk.cyan(settings.url)}] extracted ${chalk.green('all')} questions`)
             }).catch(code => {
                 console.error(`[${chalk.cyan(settings.url)}] responded with ${chalk.red(code)}`)
             })
